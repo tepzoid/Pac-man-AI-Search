11d2d07280b75dd723896cc1ae8c8845fc802310
Lleyton-Martin <l.v.martin@wustl.edu>
2023-02-21 16:18:40 -0600

    First commit

--- search.py
+++ search.py
@@ -87,6 +87,7 @@ def depthFirstSearch(problem):
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
     "*** YOUR CODE HERE ***"
+    print("Test:")
     util.raiseNotDefined()
 
 def breadthFirstSearch(problem):
